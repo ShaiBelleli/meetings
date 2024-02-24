c04eae22d9e3a7eb570f64f7b4d4e9abc999257f
ShaiBelleli <the.baduk.player@gmail.com>
2024-02-24 13:47:20 +0200

    refactor

--- src/main/java/com/shaibal/meetings/constants/ContextConstants.java
+++ src/main/java/com/shaibal/meetings/constants/ContextConstants.java
@@ -1,9 +1,7 @@
 package com.shaibal.meetings.constants;
 
 public class ContextConstants {
-    public static final String GET_ALL_MEETINGS_RESPONSE = "getAllMeetingsResponse";
-    public static final String MEETING_REQUEST_DTO_TO_ADD = "meetingRequestDTOToAdd";
-    public static final String MEETING_ADDED_RESPONSE = "meetingAddedResponse";
-    public static final String MEETING_ID_TO_DELETE = "meetingIdToDelete";
-    public static final String MEETING_ID_TO_ATTEND = "meetingIdToAttend";
+
+    public static final String MEETING_ID = "meetingId";
+    public static final String MEETING_REQUEST_DTO = "meetingRequestDTO";
 }
